14b3a13e1c6803ac946099c6202158889686fafe
jlbaldwin <jbcoug@gmail.com>
2019-09-04 10:48:23 -0700

    updated state array

--- src/data.js
+++ src/data.js
@@ -6,27 +6,124 @@ const teamData = [
         id: 1,
         name: "Arsenal",
         teamImg: "/images/arsenal.png",
-        pointsByWeek: [1, 2, 2, 5]
+        pointsByWeek: [3,6,6,6]
     },
     {
         id: 2,
         name: "Aston Villa",
         teamImg: "/images/astonvilla.png",
-        pointsByWeek: [0, 3, 3, 3]
+        pointsByWeek: [0,0,3,3]
     },
     {
         id: 3,
-        name: "Chelsea",
-        teamImg: "/images/chelsea.png",
-        pointsByWeek: [0, 1, 4, 5]
+        name: "Bournemouth",
+        teamImg: "/images/bournemouth.png",
+        pointsByWeek: [1,4,4,4]
     },
     {
         id: 4,
+        name: "Brighton & Hove Albion",
+        teamImg: "/images/brightonha.png",
+        pointsByWeek: [3,4,4,4]
+    },
+    {
+        id: 5,
+        name: "Burnley",
+        teamImg: "/images/burnley.png",
+        pointsByWeek: [3,3,4,4]
+    },
+    {
+        id: 6,
+        name: "Chelsea",
+        teamImg: "/images/chelsea.png",
+        pointsByWeek: [0,1,4,5]
+    },
+    {
+        id: 7,
+        name: "Crystal Palace",
+        teamImg: "/images/crystalpalace.png",
+        pointsByWeek: [1,1,4,7]
+    },
+    {
+        id: 8,
+        name: "Everton",
+        teamImg: "/images/everton.png",
+        pointsByWeek: [1,4,4,4]
+    },
+    {
+        id: 9,
+        name: "Leicester City",
+        teamImg: "/images/leicester.png",
+        pointsByWeek: [1,2,5,8]
+    },
+    {
+        id: 10,
+        name: "Liverpool",
+        teamImg: "/images/liverpool.png",
+        pointsByWeek: [3,6,9,12]
+    },
+    {
+        id: 11,
         name: "Manchester City",
         teamImg: "/images/mancity.png",
-        pointsByWeek: [3, 4, 7, 8]
-    }
+        pointsByWeek: [3,4,7,10]
+    },
+    {
+        id: 12,
+        name: "Manchester United",
+        teamImg: "/images/manunited.png",
+        pointsByWeek: [3,3,4,5]
+    },
+    {
+        id: 13,
+        name: "Newcastle United",
+        teamImg: "/images/newcastle.png",
+        pointsByWeek: [0,0,3,4]
+    },
+    {
+        id: 14,
+        name: "Norwich City",
+        teamImg: "/images/norwich.png",
+        pointsByWeek: [0,3,3,3]
+    },
+    {
+        id: 15,
+        name: "Sheffield United",
+        teamImg: "/images/sheffield.png",
+        pointsByWeek: [1,1,4,5]
+    },
+    {
+        id: 16,
+        name: "Southampton",
+        teamImg: "/images/southampton.png",
+        pointsByWeek: [0,0,3,4]
+    },
+    {
+        id: 17,
+        name: "Tottenham Hotspur",
+        teamImg: "/images/tottenham.png",
+        pointsByWeek: [3,4,4,4]
+    },
+    {
+        id: 18,
+        name: "Watford",
+        teamImg: "/images/watford.png",
+        pointsByWeek: [0,0,0,1]
+    },
+    {
+        id: 19,
+        name: "West Ham United",
+        teamImg: "/images/westham.png",
+        pointsByWeek: [0,1,4,7]
+    },
+    {
+        id: 20,
+        name: "Wolverhampton Wanderers",
+        teamImg: "/images/wolverhampton.png",
+        pointsByWeek: [1,2,3,3]
+    }    
 ]
+
 
 
 export default teamData
