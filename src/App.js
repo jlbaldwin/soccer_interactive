d618c500b4a980615e7c19a259843a6a397f070c
jlbaldwin <jbcoug@gmail.com>
2019-12-03 15:03:27 -0800

    updated standings

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ class App extends Component {
       league: 'Premier League',
       year: '2019',
       teamCt: 20, 
-      orgIterator: 12,
-      iterator: 12, 
+      orgIterator: 14,
+      iterator: 14, 
     }
 
     this.initialAnimation = this.initialAnimation.bind(this)
